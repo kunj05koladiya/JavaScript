f5a4aabe8e9d178461bef77c4645318db1825e12
kunj <kunj.koladiya1972@gmail.com>
2022-11-16 21:59:19 -0800

    commit

---
+++ operator2.js
@@ -0,0 +1,76 @@
+// console.log("0"==false);
+// console.log(false=="0");
+// console.log(false==0);
+// console.log(0==false);
+// console.log(false==="0");
+// console.log(false===0);
+
+// var a=1,b=2
+// console.log(a+ +b);
+// console.log(a + + b);   // a+ (+b)
+// console.log(a+ -b);
+// console.log(b-+a);
+
+// var a=3,b=3,c;
+// console.log(a++);
+// console.log(++a);
+// c = --a && b++;
+// console.log(a,"-",b,"-",c);
+
+
+// --------     XOR -> exclusive OR ^   ----------
+// console.log(1^1);        //0
+// console.log(1^0);        //1
+// console.log(0^1);        //1
+// console.log(0^0);        //0
+
+// console.log(1^0^1);         //0
+// console.log(0^1^0);         //1
+
+
+// var a=10,b=30;
+// var str1="welcome" +a + "to my home"+" str1 ";
+// var str2='welcome' +a + 'to my home'+' str2 ';
+// var str3=`welcome ${a} ${b} 
+//  to my home ${"str3"}`;      // backtick ES6 - template literal
+
+// console.log(str1);
+// console.log(str2);
+// console.log(str3);
+
+
+// varible
+// declare varible using -> 1.var 2.let 3.const
+
+// const a=15;
+// a=20;
+// console.log(a);
+
+// let a=10;
+// console.log(a);
+
+// console.log(a+ '10');
+// console.log(typeof(a+ '10'));
+// console.log(a+ true);
+// console.log(typeof(a+true));
+// console.log(a+ false+ '10');
+// console.log(typeof(a+ false +'10'));
+
+// let a=10;
+// let b="10" + "a"
+// let b="10" + a
+// console.log(b);
+
+
+// 1.undefined   2.null   3.NaN
+
+// console.log(undefined==undefined);
+// console.log(undefined===undefined);
+
+// console.log(null==null);
+// console.log(null===null);
+
+// console.log(NaN==NaN);
+// console.log(NaN===NaN);
+
+
